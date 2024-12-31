3e213c6d796cd985e9a4ef1027ac1be4065d1617
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2024-12-31 12:17:52 +0530

    project init... still working

--- client/components/transactions/projectInit.tsx
+++ client/components/transactions/projectInit.tsx
@@ -2,10 +2,10 @@ import { NETWORK } from '@/config/lucid';
 import { ArbitratorTokenValidator, ConfigDatumHolderValidator, HoldingContractValidator, identificationPolicyid, MilestoneMINTValidator, MilestoneSpendValidator, ProjectInitiateValidator } from '@/config/scripts/scripts';
 import { useWallet } from '@/contexts/walletContext'
 import { ConfigDatum, ConfigDatumSchema, ProjectDatum } from '@/types/cardano';
-import { Data, fromText, mintingPolicyToId, paymentCredentialOf, Script, SpendingValidator, Validator, validatorToAddress, validatorToScriptHash } from '@lucid-evolution/lucid';
+import { Data, fromText, MintingPolicy, mintingPolicyToId, paymentCredentialOf, Script, SpendingValidator, Validator, validatorToAddress, validatorToScriptHash } from '@lucid-evolution/lucid';
 import React from 'react'
 import { Button } from '../ui/button';
-import { getAddress, getPolicyId, refUtxo } from '@/libs/utils';
+import { getAddress, getPolicyId, handleError, refUtxo } from '@/libs/utils';
 
 export default function ProjectInitiate() {
     const [WalletConnection] = useWallet()
@@ -16,50 +16,48 @@ export default function ProjectInitiate() {
     async function createProject() {
         if (!lucid || !address) throw "Uninitialized Lucid!!!";
         const contractAddress = getAddress(ProjectInitiateValidator)
-        const mintingValidator: SpendingValidator = ProjectInitiateValidator();
+        const mintingValidator: MintingPolicy = ProjectInitiateValidator();
         const policyID = getPolicyId(ProjectInitiateValidator);
 
-
-        const datum: ProjectDatum = {
-            title: fromText("firstProject"),
-            pay: 5_000_000n,
-            developer: null,
-            client: paymentCredentialOf(address).hash,
-            milestones: [{
-                name: fromText("milestone1"),
-                status: false,
+        try {
+            const datum: ProjectDatum = {
+                title: fromText("firstProject"),
                 pay: 5_000_000n,
-            }],
-            current_milestone: null,
-            next_milestone: null,
+                developer: null,
+                client: paymentCredentialOf(address).hash,
+                milestones: [],
+                current_milestone: null,
+                next_milestone: null,
+            }
+
+            const clt_assetname = fromText("clt_") + datum.title
+            const dev_assetname = fromText("dev_") + datum.title
+            const clt_token = { [policyID + clt_assetname]: 1n }
+            const dev_token = { [policyID + dev_assetname]: 1n }
+
+            const ref_utxo = await refUtxo(lucid)
+
+
+            const tx = await lucid
+                .newTx()
+                .readFrom(ref_utxo)
+                .pay.ToContract(
+                    contractAddress,
+                    { kind: "inline", value: Data.to(datum, ProjectDatum) },
+                    { lovelace: 5_000_000n, ...dev_token }
+                )
+                .mintAssets({ ...clt_token, ...dev_token }, Data.void())
+                .attach.MintingPolicy(mintingValidator)
+                .complete();
+
+            console.log(tx, "complete")
+            const signed = await tx.sign.withWallet().complete();
+            const txHash = await signed.submit();
+            console.log("txHash: ", txHash);
+        } catch (error) {
+            handleError(error);
         }
 
-        console.log(datum)
-        console.log(Data.to(datum, ProjectDatum))
-        const clt_assetname = fromText("clt_") + datum.title
-        const dev_assetname = fromText("dev_") + datum.title
-        const clt_token = { [policyID + clt_assetname]: 1n }
-        const dev_token = { [policyID + dev_assetname]: 1n }
-
-        const ref_utxo = await refUtxo(lucid)
-
-
-        const tx = await lucid
-            .newTx()
-            .readFrom(ref_utxo)
-            .pay.ToAddressWithData(
-                contractAddress,
-                { kind: "inline", value: Data.to(datum, ProjectDatum) },
-                { lovelace: 5_000_000n, ...dev_token }
-            )
-            .mintAssets({ ...clt_token, ...dev_token }, Data.void())
-            .attach.MintingPolicy(mintingValidator)
-            .complete();
-
-
-        const signed = await tx.sign.withWallet().complete();
-        const txHash = await signed.submit();
-        console.log("txHash: ", txHash);
     }
 
     return (
