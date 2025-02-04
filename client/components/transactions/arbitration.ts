e0cb4c4d6af67d2e4f454e76dfc5e5042dd75a3c
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-02-04 17:16:24 +0530

    sign with seed and related changes

--- client/components/transactions/arbitration.ts
+++ client/components/transactions/arbitration.ts
@@ -25,7 +25,7 @@ export async function arbitration(
   walletConnection: WalletConnection,
   utxo: UTxO,
   isDev: boolean,
-  arbitrationLink: string,
+  arbitrationLink: string
 ) {
   const { lucid, address } = walletConnection;
   if (!lucid || !address) throw "Uninitialized Lucid!!!";
@@ -44,7 +44,7 @@ export async function arbitration(
 
     const ref_utxo = await refUtxo(lucid);
     const UTxO_Talendro = await lucid.utxoByUnit(
-      TALENDROPID + fromText(address.slice(-10)),
+      TALENDROPID + fromText(address.slice(-10))
     ); //talendroPolicyID+assetName assetname is user address
     const redeemer = Data.to("Arbitrator", ProjectRedeemer);
     const tx = await lucid
@@ -54,7 +54,7 @@ export async function arbitration(
       .pay.ToAddressWithData(
         ARBITRATIONADDR,
         { kind: "inline", value: Data.to(arbDatum, ArbitratorDatum) },
-        { lovelace: datum.pay as bigint, ...projecttoken },
+        { lovelace: datum.pay as bigint, ...projecttoken }
       )
       .attach.SpendingValidator(HoldingContractValidator())
       .complete();
@@ -73,7 +73,7 @@ export async function arbitration(
 export async function ArbitratorAction(
   walletConnection: WalletConnection,
   utxo: UTxO,
-  devAtFault: boolean,
+  devAtFault: boolean
 ) {
   const { lucid, address } = walletConnection;
   if (!lucid || !address) throw new Error("Uninitialized Lucid!!!");
@@ -83,7 +83,7 @@ export async function ArbitratorAction(
     const currentDatum = Data.castFrom(data, ArbitratorDatum);
     const cltAddress = keyHashtoAddress(currentDatum.project_datum.client);
     const devAddress = keyHashtoAddress(
-      currentDatum.project_datum.developer as string,
+      currentDatum.project_datum.developer as string
     );
     let PaytoAddress = devAtFault ? cltAddress : devAddress;
     let AtFaultAddress = devAtFault ? devAddress : cltAddress;
@@ -94,7 +94,7 @@ export async function ArbitratorAction(
     const ref_utxo = await refUtxo(lucid);
     const UTxO_Arbitrator = await lucid.utxosAtWithUnit(
       address,
-      ARBITRATORPID + fromText(address.slice(-10)),
+      ARBITRATORPID + fromText(address.slice(-10))
     );
     const redeemer: ArbitratorRedeemer = { payto: devAtFault ? 0n : 1n };
     console.log(ARBITRATORPID);
@@ -103,7 +103,7 @@ export async function ArbitratorAction(
       .readFrom(ref_utxo)
       .collectFrom(
         [...UTxO_Arbitrator, utxo, stakedUtxo],
-        Data.to(redeemer, ArbitratorRedeemer),
+        Data.to(redeemer, ArbitratorRedeemer)
       )
       .pay.ToAddress(PaytoAddress, {
         lovelace: currentDatum.project_datum.pay as bigint,
@@ -115,9 +115,11 @@ export async function ArbitratorAction(
       .addSigner(STAKEADDRESS)
       .complete();
 
-    const stakeSigned = await StakeWallet(tx);
-    const signed = await stakeSigned.sign.withWallet().complete();
-    const txHash = await signed.submit();
+    const userSigned = await tx.sign.withWallet();
+    const stakeSigned = await (
+      await StakeWallet(walletConnection, userSigned)
+    ).complete();
+    const txHash = await stakeSigned.submit();
     console.log("txHash: ", txHash);
 
     return { data: txHash, error: null };
--- client/config/systemWallet.ts
+++ client/config/systemWallet.ts
@@ -1,13 +1,28 @@
-import { TxSignBuilder } from "@lucid-evolution/lucid";
+import {
+  makeWalletFromSeed,
+  TxSignBuilder,
+  Wallet,
+  WalletApi,
+} from "@lucid-evolution/lucid";
 import { PRIVATEKEY, STAKESEED } from ".";
 import { signWithPrivateKey, signWithSeed } from "@/lib/utils";
+import { WalletConnection } from "@/context/walletContext";
+import { NETWORK, PROVIDER } from "./lucid";
 
 export async function SystemWallet(tx: TxSignBuilder) {
   const txSystemSigned = await signWithPrivateKey(tx, PRIVATEKEY);
   return txSystemSigned;
 }
 
-export async function StakeWallet(tx: TxSignBuilder) {
+export async function StakeWallet(
+  walletConnection: WalletConnection,
+  tx: TxSignBuilder
+) {
+  const { lucid, wallet } = walletConnection;
+  await lucid?.selectWallet.fromSeed(STAKESEED, {
+    addressType: "Base",
+  });
   const txSystemSigned = await signWithSeed(tx, STAKESEED);
+  await lucid?.selectWallet.fromAPI((await wallet?.enable()) as WalletApi);
   return txSystemSigned;
 }
