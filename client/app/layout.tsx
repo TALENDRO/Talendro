5ef44529a3dc3d0cbe4737b8194eb0829e55ca77
Cryptonean <faizanvit8@gmail.com>
2025-02-07 19:53:29 +0530

    authcontext

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -9,6 +9,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans, fontComfortaa } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import { AuthProvider } from "@/context/authcontext";
 
 export const metadata: Metadata = {
   title: {
@@ -35,24 +36,26 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      <head />
-      <body
-        className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
-          fontComfortaa.variable,
-        )}
-      >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto pt-16 px-6 flex-grow">
-              {children}
-            </main>
-          </div>
-          <Toaster richColors />
-        </Providers>
-      </body>
+      <AuthProvider>
+        <head />
+        <body
+          className={clsx(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable,
+            fontComfortaa.variable
+          )}
+        >
+          <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+            <div className="relative flex flex-col h-screen">
+              <Navbar />
+              <main className="container mx-auto pt-16 px-6 flex-grow">
+                {children}
+              </main>
+            </div>
+            <Toaster richColors />
+          </Providers>
+        </body>
+      </AuthProvider>
     </html>
   );
 }
