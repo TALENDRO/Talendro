ab53558264fc9958277c5f3cd4c17870d542e0bb
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-02-08 10:33:51 +0530

    calling constants in Home

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,34 @@
 "use client";
 import ClientHome from "./client";
+import {
+  IDENTIFICATIONPID,
+  MILESTONEPID,
+  MILESTONEADDR,
+  HOLDINGADDR,
+  PROJECTINITADDR,
+  ARBITRATORPID,
+  ARBITRATIONADDR,
+  TALENDROPID,
+  STAKEADDRESS,
+} from "@/config";
+import { ConfigDatum } from "@/types/cardano";
+import { paymentCredentialOf, stakeCredentialOf } from "@lucid-evolution/lucid";
 
 export default function Home() {
+  const CONFIGDATUM: ConfigDatum = {
+    identification_nft: IDENTIFICATIONPID,
+    milestone_contract_policy: MILESTONEPID,
+    milestone_contract_address: paymentCredentialOf(MILESTONEADDR).hash,
+    holding_contract: paymentCredentialOf(HOLDINGADDR).hash,
+    projectinit_contract: paymentCredentialOf(PROJECTINITADDR).hash,
+    arbitrator_nft: ARBITRATORPID,
+    arbitrator_contract: paymentCredentialOf(ARBITRATIONADDR).hash,
+    talendrouser_nft: TALENDROPID,
+    stake_address: [
+      paymentCredentialOf(STAKEADDRESS).hash,
+      stakeCredentialOf(STAKEADDRESS).hash,
+    ],
+    stake_amount: 100_000_000n,
+  };
   return <ClientHome />;
 }
