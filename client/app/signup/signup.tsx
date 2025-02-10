88c856686e16b1f2093ebf0d3aa8e989c051b260
Cryptonean <faizanvit8@gmail.com>
2025-02-10 14:20:57 +0530

    register

--- client/app/signup/signup.tsx
+++ client/app/signup/signup.tsx
@@ -1,10 +1,17 @@
+import Login from "@/components/login";
 import { LoginForm } from "@/components/login-form";
+import Register from "@/components/register";
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <button>
+          <Login />
+        </button>
+        {/* <button>
+          <Register />
+        </button> */}
       </div>
     </div>
   );
